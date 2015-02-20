dba3a77892b319e44ecff75bd3e3457581675135
Kenny Lee Sin Cheong <kenny.lee28@gmail.com>
2015-02-20 15:04:46 -0500

    Trying out helm vs ido

--- init.el
+++ init.el
@@ -277,7 +277,6 @@ Define colors in 'blink-cursor-colors'."
 (global-set-key (kbd "<f8>") 'window-configuration-to-register)
 (global-set-key (kbd "<f9>") 'jump-to-register)
 
-
 ;; GUI
 ;; (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -288,8 +287,17 @@ Define colors in 'blink-cursor-colors'."
 (column-number-mode t)
 (display-battery-mode t)
 
-;; Minibuffer handling
+;; Minibuffer handling (Ido Mode)
 (ido-mode t)
+(setq ido-enable-flex-matching t)
+(ido-everywhere)
+;; Because using arrow keys is annoying
+(define-key ido-file-completion-map (kbd "C-p") 'ido-prev-match)
+(define-key ido-file-completion-map (kbd "C-n") 'ido-next-match)
+(define-key ido-file-dir-completion-map (kbd "C-p") 'ido-prev-match)
+(define-key ido-file-dir-completion-map (kbd "C-n") 'ido-next-match)
+(define-key ido-buffer-completion-map (kbd "C-p") 'ido-prev-match)
+(define-key ido-buffer-completion-map (kbd "C-n") 'ido-next-match)
 
 ;; recentf Setup
 (recentf-mode t)
@@ -491,6 +499,67 @@ Define colors in 'blink-cursor-colors'."
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
+;; Helm Stuff
+;; ----------
+(require 'helm)
+(require 'helm-config)
 
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+;; rebind tab to run persistent action
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+;; make TAB works in terminal
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
+;; list actions using C-z
+(define-key helm-map (kbd "C-z")  'helm-select-action)
+
+(when (executable-find "curl")
+      (setq helm-google-suggest-use-curl-p t))
+
+;; 1. open helm buffer inside current window, not occupy whole other window
+;; 2. move to end or beginning of source when reaching top or bottom of source.
+;; 3. search for library in `require' and `declare-function' sexp.
+;; 4. scroll 8 lines other window using M-<next>/M-<prior>
+;; 5. use recentf
+(setq helm-split-window-in-side-p           t 
+      helm-move-to-line-cycle-in-source     t 
+      helm-ff-search-library-in-sexp        t 
+      helm-scroll-amount                    8 
+      helm-ff-file-name-history-use-recentf t)
+
+(helm-autoresize-mode t)
+(setq helm-M-x-fuzzy-match t
+      helm-buffers-fuzzy-matching t
+      helm-recentf-fuzzy-match t
+      helm-semantic-fuzzy-match t
+      helm-imenu-fuzzy-match t)
+
+(defun toggle-helm-mode-on ()
+  "Toggle helm-mode on."
+  (interactive)
+  (progn (global-set-key (kbd "M-x") 'helm-M-x)
+         (global-set-key (kbd "M-y") 'helm-show-kill-ring)
+         (global-set-key (kbd "C-x b") 'helm-mini)
+         (global-set-key (kbd "C-x C-f") 'helm-find-files)
+         (helm-mode t)))
+
+(defun toggle-helm-mode-off ()
+  "Toggle helm-mode off."
+  (interactive)
+  (progn (global-set-key (kbd "M-x") 'execute-extended-command)
+         (global-set-key (kbd "M-y") 'yank-pop)
+         (global-set-key (kbd "C-x b") 'ido-switch-buffer)
+         (global-set-key (kbd "C-x C-f") 'ido-find-file)
+         (helm-mode 0)))
+
+(defun toggle-helm-mode ()
+  "Toggle helm-mode on/off."
+  (interactive)
+  (if helm-mode (toggle-helm-mode-off)
+      (toggle-helm-mode-on)))
+(global-set-key (kbd "<f12>") 'toggle-helm-mode)
 
 ;;; init.el ends here
