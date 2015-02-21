f331bef87cfe47b70a61d4227f9cd4f8c541be0a
Kenny Lee Sin Cheong <kenny.lee28@gmail.com>
2015-02-21 16:48:59 -0500

    helm-gtags

--- init.el
+++ init.el
@@ -531,6 +531,8 @@ Define colors in 'blink-cursor-colors'."
       helm-ff-file-name-history-use-recentf t)
 
 (helm-autoresize-mode t)
+
+;; Fuzzy matching
 (setq helm-M-x-fuzzy-match t
       helm-buffers-fuzzy-matching t
       helm-recentf-fuzzy-match t
@@ -561,5 +563,29 @@ Define colors in 'blink-cursor-colors'."
   (if helm-mode (toggle-helm-mode-off)
       (toggle-helm-mode-on)))
 (global-set-key (kbd "<f12>") 'toggle-helm-mode)
+
+;; Helm-gtags
+;; ----------
+(setq helm-gtags-ignore-case t
+      helm-gtags-auto-update t
+      helm-gtags-use-input-at-cursor t
+      helm-gtags-pulse-at-cursor t
+      helm-gtags-prefix-key "\C-cg"
+      helm-gtags-suggested-key-mapping t)
+
+(require 'helm-gtags)
+;; Enable helm-gtags-mode
+(add-hook 'dired-mode-hook 'helm-gtags-mode)
+(add-hook 'eshell-mode-hook 'helm-gtags-mode)
+(add-hook 'c-mode-hook 'helm-gtags-mode)
+(add-hook 'c++-mode-hook 'helm-gtags-mode)
+(add-hook 'asm-mode-hook 'helm-gtags-mode)
+
+(define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+(define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
+(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
 
 ;;; init.el ends here
