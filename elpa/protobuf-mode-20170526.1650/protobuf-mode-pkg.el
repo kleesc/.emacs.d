d9fd34d14b574d9a9da7b8461ab478f05bf5ebde
Kenny Lee Sin Cheong <kenny.lee28@gmail.com>
2020-05-07 20:17:52 -0400

    Package updates

---
+++ elpa/protobuf-mode-20170526.1650/protobuf-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "protobuf-mode" "20170526.1650" "major mode for editing protocol buffers." 'nil :commit "204f99488ce1ef74565239cf3963111ae4c774b7" :keywords '("google" "protobuf" "languages") :authors '(("Alexandre Vassalotti" . "alexandre@peadrop.com")) :maintainer '("Alexandre Vassalotti" . "alexandre@peadrop.com"))
